c4be8f1a065270fd988d438be180f828767006f1
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2022-01-04 17:18:33 -0500

    Add tests for httpClientHandler.

--- test/mocha/10-test.js
+++ test/mocha/10-test.js
@@ -3,7 +3,8 @@
  */
 'use strict';
 
-const {documentLoader, jsonLdDocumentLoader} =
+const {util: {uuid}} = require('bedrock');
+const {documentLoader, jsonLdDocumentLoader, httpClientHandler} =
   require('bedrock-jsonld-document-loader');
 
 describe('bedrock-jsonld-document-loader', () => {
@@ -43,5 +44,74 @@ describe('bedrock-jsonld-document-loader', () => {
       documentUrl,
       tag: 'static'
     });
+  });
+});
+
+describe('httpClientHandler', () => {
+  it('properly gets document', async () => {
+    let result;
+    let error;
+    const documentUrl = 'https://httpbin.org/json';
+
+    try {
+      result = await httpClientHandler.get({url: documentUrl});
+    } catch(e) {
+      error = e;
+    }
+    assertNoError(error);
+    should.exist(result);
+    result.should.eql({
+      contextUrl: null,
+      document: {
+        slideshow: {
+          author: 'Yours Truly',
+          date: 'date of publication',
+          slides: [
+            {
+              title: 'Wake up to WonderWidgets!',
+              type: 'all'
+            },
+            {
+              items: [
+                'Why <em>WonderWidgets</em> are great',
+                'Who <em>buys</em> WonderWidgets'
+              ],
+              title: 'Overview',
+              type: 'all'
+            }
+          ],
+          title: 'Sample Slide Show'
+        }
+      },
+      documentUrl: 'https://httpbin.org/json'
+    });
+  });
+  it('throws error if url does not start with "http"', async () => {
+    let result;
+    let error;
+    const url = `urn:zcap:${uuid()}`;
+
+    try {
+      result = await httpClientHandler.get({url});
+    } catch(e) {
+      error = e;
+    }
+    should.exist(error);
+    should.not.exist(result);
+    error.message.should.equal('NotFoundError');
+  });
+  it('throws error if document is not found', async () => {
+    let result;
+    let error;
+    const documentUrl = 'http://invalid.org/json';
+
+    try {
+      result = await httpClientHandler.get({url: documentUrl});
+    } catch(e) {
+      error = e;
+    }
+    should.exist(error);
+    should.not.exist(result);
+    error.message.should.equal('NotFoundError');
   });
 });
