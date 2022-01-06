650cf2d9e956073fd8b0543b415abc6e77cc1bf7
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2022-01-06 12:32:45 -0500

    Remove log and fix `https-agent` config.

--- test/test.config.js
+++ test/test.config.js
@@ -6,8 +6,7 @@
 const {config} = require('bedrock');
 const path = require('path');
 
-const cfg = config['https-agent'] = {};
 // set this to false to ignore SSL errors in development
-cfg.rejectUnauthorized = false;
+config['https-agent'].rejectUnauthorized = false;
 
 config.mocha.tests.push(path.join(__dirname, 'mocha'));
